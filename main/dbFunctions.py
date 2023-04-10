12a366d272d7b937db9e2db6a7642cf5e935160d
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-04-10 20:40:35 -0300

    Added comments to the functions

--- main/dbFunctions.py
+++ main/dbFunctions.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("main.db")
 c = conn.cursor()
 
-def get_encrypted_password(website, username):
+def get_encrypted_password(website: str, username: str):
     with conn:
         c.execute("SELECT * FROM main WHERE website=:website AND username=:username", 
                                         {'website': website, 'username': username})
@@ -14,7 +14,7 @@ def get_all_passwords():
         c.execute("SELECT * FROM main")
     return c.fetchall()
 
-def add_password(encrypted_password, website, username):
+def add_password(encrypted_password: str, website: str, username: str):
         with conn: 
             c.execute("INSERT INTO main VALUES(:password, :website, :username)", 
                                                        {'password': encrypted_password, 
@@ -22,20 +22,20 @@ def add_password(encrypted_password, website, username):
                                                         'username': username})
                                                           
     
-def edit_password(new_password, website):
+def edit_password(new_password: str, website: str):
     with conn:
         c.execute("UPDATE main SET password=:new_password WHERE website=:website", {'new_password': new_password, 'website': website})
     
 
-def edit_username(new_username, website):
+def edit_username(new_username: str, website: str):
     with conn:
         c.execute("UPDATE main SET username=:new_username WHERE website=:website", {'new_username': new_username, 'website': website})
 
-def delete_entry(website, username):
+def delete_entry(website: str, username: str):
     with conn:
         c.execute("DELETE from main WHERE website=:website AND username=:username", {"website": website, "username": username})
 
-def check_existing_entry(website, username):
+def check_existing_entry(website: str, username: str):
     """Returns True if there is an existing entry, false otherwise"""
     with conn:
         c.execute("SELECT * FROM main WHERE website=:website AND username=:username", 
--- main/dbMemory.py
+++ main/dbMemory.py
@@ -3,11 +3,19 @@ from sqlite3 import connect
 conn = connect("main.db")
 c = conn.cursor()
 
-def send_to_temp(website, username):
+def send_to_temp(website: str, username: str):
+    '''
+    Sends the website and username data to a temporary database to then retrieve them in the 
+    next instance, the ShowPassword menu; in this menu the algorithm calls the db to ask for the password
+    with the website and username provided from the temp db.
+    '''
     with conn: 
         c.execute("INSERT INTO temp VALUES (:website, :username)", {'website': website, 'username':username})
 
 def get_site():
+    '''
+    Gets entry data from the temp database to then ask the main database for the password.
+    '''
     with conn:
         c.execute("SELECT * FROM temp")
         result = c.fetchone()        
